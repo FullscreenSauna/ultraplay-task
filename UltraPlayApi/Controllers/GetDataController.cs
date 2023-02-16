098b2ab36b828562522194b91423aa8a631a2a38
Aleksandar Dzhekov <aleksandar.dzhekov@gmail.com>
2023-02-16 17:00:50 +0200

    refactor db updates to not throw errors when inserting beacause of duplicated unique constraint

--- UltraPlayApi/Controllers/GetDataController.cs
+++ UltraPlayApi/Controllers/GetDataController.cs
@@ -2,6 +2,8 @@
 using UltraPlayApi.Mappers;
 using UltraPlayApi.Interfaces;
 using UltraPlayApi.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace UltraPlayApi.Controllers
 {
@@ -37,10 +39,50 @@ namespace UltraPlayApi.Controllers
 
                 _events = FeedMapper.MapFromXmlString(xml);
 
-                _context.AddRange(_events);
-
-                _context.SaveChanges();
+                SaveDbChanges(_events);
             }
+        }
+
+        private void SaveDbChanges(IEnumerable<IEvent> events)
+        {
+            foreach (var _event in events)
+            {
+                foreach (var match in _event.Matches)
+                {
+                    var existingMatch = _context.Matches.FirstOrDefault(e => e.ExternalId == match.ExternalId);
+
+                    if (existingMatch == null)
+                    {
+                       var newMatch = match;
+
+                        _context.Matches.Add(newMatch);
+                    }
+                    else
+                    {
+                        existingMatch.MatchType = match.MatchType;
+                        existingMatch.StartDate = match.StartDate;
+                    }
+
+                    foreach (var bet in match.Bets)
+                    {
+                        foreach (var odd in bet.Odds)
+                        {
+                            var existingOdd = _context.Odds.FirstOrDefault(o => o.ExternalId == _event.ExternalId);
+
+                            if (existingOdd == null)
+                            {
+                                var newOdd = odd;
+                            }
+                            else
+                            {
+                                existingOdd.Value = odd.Value;
+                            }
+                        }
+                    }
+                }
+            }
+
+            _context.SaveChanges();
         }
     }
 }
