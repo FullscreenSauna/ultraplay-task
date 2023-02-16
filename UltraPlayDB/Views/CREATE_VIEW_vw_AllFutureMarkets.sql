ccaa5c1db5ec43e956069f6ad9e2325d8ead6c28
Aleksandar Dzhekov <aleksandar.dzhekov@gmail.com>
2023-02-16 19:03:14 +0200

    add view creation scripts

---
+++ UltraPlayDB/Views/CREATE_VIEW_vw_AllFutureMarkets.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW vw_AllFutureMarkets AS
+SELECT 
+    m.Name AS MatchName,
+    m.StartDate,
+    b.Name AS MarketName,
+    o.Name AS OddName,
+    COALESCE(MAX(CASE WHEN o.SpecialBetValue IS NOT NULL THEN o.SpecialBetValue ELSE NULL END), MAX(o.Value)) AS Odds
+FROM Matches m
+INNER JOIN Bets b ON b.MatchId = m.Id
+INNER JOIN Odds o ON o.BetId = b.Id
+WHERE DATEADD(HH, 24, m.StartDate) <= DATEADD(HH, 24, GETDATE())
+AND b.IsLive = 1
+AND b.Name IN ('Match Winner', 'Map Advantage', 'Total Maps Played')
+GROUP BY m.Name,
+         m.StartDate,
+         b.Name,
+         o.Name
---
+++ UltraPlayDB/Views/CREATE_VIEW_vw_AllMatches.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW vw_AllMatches AS
+SELECT m.Name as MatchName,
+	   m.StartDate,
+	   b.Name as MarketName,
+	   o.Name as OddName,
+	   o.Value,
+	   o.SpecialBetValue,
+	   m.ExternalId
+FROM Matches m
+INNER JOIN Bets b ON b.MatchId = m.Id
+INNER JOIN Odds o ON o.BetId = b.Id
